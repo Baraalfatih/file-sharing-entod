8caf854943e1c24c07f4236a1ec37250796b67fa
Baraalfatih <96493914+Baraalfatih@users.noreply.github.com>
2022-01-01 03:24:27 +0700

    Update and rename plugins/cbb.py to plugin/cbb.py

--- plugin/cbb.py
+++ plugin/cbb.py
@@ -1,4 +1,4 @@
-#(©) KEK-Projects
+#(©) JACK-OFFICCE
 
 from pyrogram import __version__
 from bot import Bot
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/skuyykek69/File-Sharing-Bot'>Click here</a>\n○ Channel : @KEK_Projects\n○ Support Group : @marikitamerapat</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/Baraalfatih/File-Sharing-entod'>Click here</a>\n○ Channel : @SOFTWAREQUOTEXX\n○ Support Group : @affiliatesidn</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
